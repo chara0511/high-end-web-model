e2e2ea185645081efe2b8cae7aab3079584deca2
Chara- <escor05_11@hotmail.com>
2019-11-26 03:21:04 -0500

    add section: team

--- js/main.js
+++ js/main.js
@@ -24,6 +24,38 @@ $(document).ready(function() {
     maxSlides: 1,
     speed: 1200
   });
+
+  if($(".swiper-container").hasClass("team-member-slider")){
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      allowTouchMove: true,
+      loop: true,
+      centeredSlides: true,
+      slideToclickedslide: true,
+      effect: "coverflow",
+      grabcursor: true,
+      autoplay: false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false
+      },
+      breakpoints:{
+        767: {
+              slidesPerView: 1,
+              centeredSlides: false,
+              effect: "slide",
+        }
+      }
+    });
+  }
+
   $("#work").magnificPopup({
     delegate: "a",
     type: "image",
