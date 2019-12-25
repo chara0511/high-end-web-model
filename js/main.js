a42c1a6ccc0a13fa3ef164e1a38e79e22e02809c
Chara- <escor05_11@hotmail.com>
2019-12-24 22:19:27 -0500

    add section: portfolio (problem solved)

--- js/main.js
+++ js/main.js
@@ -63,4 +63,30 @@ $(document).ready(function() {
       enabled: true
     }
   });
+
+
+
+
+
+
+    var $gallery = $('.gallery').isotope({
+      // options
+    });
+  
+
+  // filter items on button click
+  $('.filtering').on( 'click', 'span', function() {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on( 'click', 'span', function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
 });
